836de22f91a6ef08b50df87ce63a11bf6520b9c6
Mateo Morrison Jr <mateomorrison@gmail.com>
2021-01-06 15:16:05 -0500

    mongoose integration

--- pages/api/modules/private/db/index.js
+++ pages/api/modules/private/db/index.js
@@ -1,5 +1,14 @@
-export default (req, res) => {
-  res.json({
-    "text": "text",
+import mongoose from 'mongoose';
+
+export default async () => {
+  if (mongoose.connections[0].readyState) return;
+  // Using new database connection
+  await mongoose.connect(
+      'mongodb://localhost:27017/mateo'
+      , {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   });
+  console.log('connected to mongodb')
 };
