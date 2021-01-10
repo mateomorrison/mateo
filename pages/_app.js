83dbafb32bc99529b015fb483ab91210c1555670
Mateo Mac <mateomorrison@gmail.com>
2021-01-10 15:30:39 -0500

    added state on _app.js
    let's secure a bit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import SiteHead from "./components/Head";
-import "../styles/styles.css"
+import "../styles/styles.css";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <SiteHead>
-      <Component {...pageProps} />
-    </SiteHead>
-  );
+export default function mateo({ Component, pageProps }) {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  if (isSignedIn == true) {
+    return (
+      <SiteHead>
+        <Component {...pageProps} />
+      </SiteHead>
+    );
+  }
+  if (isSignedIn == false) {
+    return <h1>hi</h1>;
+  } else {
+    setIsSignedIn(false);
+  }
 }
-
-export default MyApp;
