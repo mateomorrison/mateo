8888305b79d7c51d4266d2ddc7b5f09121139e42
Mateo Morrison Jr <mateomorrison@gmail.com>
2021-01-30 16:55:04 -0500

    no object destructing

--- pages/api/modules/private/db/index.js
+++ pages/api/modules/private/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async () => {
   if (mongoose.connections[0].readyState) return;
   // Using new database connection
-  await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect('process.env.MONGO_URI', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
