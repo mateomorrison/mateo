2034251916bbdd9a772beb003e32e5d607dd6f93
Mateo Morrison Jr <mateomorrison@gmail.com>
2021-01-30 16:53:54 -0500

    adding privacy

--- pages/api/modules/private/db/index.js
+++ pages/api/modules/private/db/index.js
@@ -1,14 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default async () => {
   if (mongoose.connections[0].readyState) return;
   // Using new database connection
-  await mongoose.connect(
-      'mongodb://localhost:27017/mateo'
-      , {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
   });
-  console.log('connected to mongodb!')
+  console.log("connected to mongodb!");
 };
